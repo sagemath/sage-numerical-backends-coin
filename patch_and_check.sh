8ac88b746b7ee24139315e621b0e932f89bab37a
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2019-12-30 20:28:34 -0500

    .github/workflows/build.yml: Simplify using new scripts

--- patch_and_check.sh
+++ patch_and_check.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
+# On Ubuntu bionic, the following fails:
+#   sage patch_into_sage_module.py
+#   sage -python patch_into_sage_module.py
+# with ImportError: No module named coin_backend.
+# Hence we workaround as follows.
 sage -c 'load("patch_into_sage_module.py")' || exit 1
 sage -c 'load("check_get_solver_with_name.py")' || exit 1

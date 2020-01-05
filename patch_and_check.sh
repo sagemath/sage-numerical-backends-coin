baef62dc3b2aac17f4f0c2e4e599c18fc0c1d812
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2020-01-05 00:37:25 -0500

    patch_and_check.sh: Fixup again for ubuntu bionic

--- patch_and_check.sh
+++ patch_and_check.sh
@@ -4,5 +4,7 @@
 #   sage -python patch_into_sage_module.py
 # with ImportError: No module named coin_backend.
 # Hence we workaround as follows.
-sage -c 'load("patch_into_sage_module.py")' || exit 1
-sage -c 'load("check_get_solver_with_name.py")' || exit 1
+sage patch_into_sage_module.py || exit 1
+## sage -c 'load("patch_into_sage_module.py")' || exit 1
+sage check_get_solver_with_name.py || exit 1
+## sage -c 'load("check_get_solver_with_name.py")' || exit 1

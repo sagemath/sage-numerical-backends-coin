ca504ad936029db5d271df079eddbf155b343f7a
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-06-29 19:21:41 -0700

    setup.py: Remove Python 3.4, 3.5

--- setup.py
+++ setup.py
@@ -102,8 +102,6 @@ setup(
                  'License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)',
                  "Programming Language :: Python",
                  'Programming Language :: Python :: 3',
-                 'Programming Language :: Python :: 3.4',
-                 'Programming Language :: Python :: 3.5',
                  'Programming Language :: Python :: 3.6',
                  'Programming Language :: Python :: 3.7',
                  'Programming Language :: Python :: 3.8',
